ad36f56758429cc2143c9c99da44326285dd6de7
asifjoardar <mdasifjoardar@gmail.com>
2022-12-21 13:22:03 +0600

    tutorial crud added

--- tutorial/src/main/java/com/asif/tutorial/repository/TutorialRepository.java
+++ tutorial/src/main/java/com/asif/tutorial/repository/TutorialRepository.java
@@ -1,2 +1,11 @@
-package com.asif.tutorial.repository;public interface TutorialRepository {
+package com.asif.tutorial.repository;
+
+import com.asif.tutorial.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
 }
